9aed3fd8ded51c1971de8ed40641d6598fbb809b
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-25 03:16:34 -0800

    feat: puppet master

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -32,6 +32,6 @@ exec  {  'create_link':
 }
 
 exec  {  'update_nginx':
-  command => "sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default",
+  command => 'sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default',
   path    => "/usr/bin"
 }
