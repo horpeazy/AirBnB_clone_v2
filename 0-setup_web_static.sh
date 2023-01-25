ce15a7d25017b5430cefbd23d6c69002ce370cbb
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 18:59:10 -0800

    feat: preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 echo "Hello world!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/server_name _/a\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
