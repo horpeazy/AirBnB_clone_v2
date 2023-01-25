c2d782b77c9819882e143b53e759baf9f8f3196a
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 19:58:20 -0800

    feat: deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ env.user = "ubuntu"
 env.hosts = ['52.201.220.244', '54.221.180.200']
 
 def do_deploy(archive_path):
+    """ deploys """
     try:
         if not os.path.exists(archive_path):
             return False
