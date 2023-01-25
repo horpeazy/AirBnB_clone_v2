15f5a9277e174cfe2ed85a5901c77f114a3d5f1f
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-25 15:53:26 -0800

    feat: clean code

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -19,10 +19,14 @@ def do_clean(number=0):
     number_local = int(local("ls -1 versions | wc -l", capture=True))
     number_local_to_delete = number_local - number
     if number_local_to_delete > 0:
-        local("ls -1tr versions | head -n " + str(number_local_to_delete) + " | xargs -I{} echo versions/{} | xargs rm")
+        first = "ls -1tr versions | head -n "
+        second = " | xargs -I{} echo versions/{} | xargs rm"
+        local("{}" + str(number_local_to_delete) + "{}".format(first, second))
 
     # clean the remote machine
     number_remote = int(run("ls -1 /data/web_static/releases | wc -l"))
     number_remote_to_delete = number_remote - number
     if number_remote_to_delete > 0:
-        run("ls -1tr /data/web_static/releases  | head -n " + str(number_remote_to_delete) + "| xargs -I{} echo /data/web_static/releases/{} | xargs rm -rf")
+        first = "ls -1tr /data/web_static/releases  | head -n "
+        secnd = "| xargs -I{} echo /data/web_static/releases/{} | xargs rm -rf"
+        run("{}" + str(number_remote_to_delete) + "{}".format(first, secnd))
