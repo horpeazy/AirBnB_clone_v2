29fdaca93a2437469f21f9c7d9038f5f17ba51ce
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 19:27:40 -0800

    feat: local pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,15 @@
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """ compress a folder """
-    local('mkdir versions')
-    directory = "versions/web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
-    result = local('tar -cvzf {} web_static'.format(directory))
-    if result.succeeded:
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        directory = "versions/web_static_{}.tgz".\
+            format(datetime.now().strftime("%Y%m%d%H%M%S"))
+        local('tar -cvzf {} web_static'.format(directory))
         return directory
-    return None
+    except Exception:
+        return None
