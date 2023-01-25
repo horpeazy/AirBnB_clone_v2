84d398f67b8dad7b68f09031137e53e49f0167c8
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 18:56:58 -0800

    feat: preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,11 @@
 # Shell script to setup a new server to serve web static
 sudo apt-get update -y
 sudo apt-get install nginx -y
-mkdir -p /data/web_static/
-mkdir /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+sudo service nginc start
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 echo "Hello world!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _/a\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit 0
