4c574ab7495d7cc923f08b2fe45bc0d0c4c674a5
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 20:37:02 -0800

    feat: deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,7 +19,9 @@ def do_deploy(archive_path):
         run('mkdir -p {}'.format(upload_folder))
         run('tar -xzf /tmp/{} -C {}'.format(archive_name, upload_folder))
         run('rm /tmp/{}'.format(archive_name))
-        run('rm -f /data/web_static/current')
+        run('mv {}/web_static/* {}'.format(upload_folder, upload_folder))
+        run('rm -rf {}/web_static'.format(upload_folder))
+        run('rm -rf /data/web_static/current')
         run('ln -sf {} /data/web_static/current'.format(upload_folder))
         return True
     except Exception as e:
