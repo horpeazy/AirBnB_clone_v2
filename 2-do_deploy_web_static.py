dd29b4bae79396759af6d94a4d85c190e232629e
horpeazy <iyamuhope.nosa@gmail.com>
2023-01-24 19:55:31 -0800

    feat: deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """ Fabric file to compress a folder """
-from fabric.api import local
+from fabric.api import run, put, env
 from datetime import datetime
 import os
 
+env.user = "ubuntu"
+env.hosts = ['52.201.220.244', '54.221.180.200']
 
-def do_pack():
-    """ compress a folder """
+def do_deploy(archive_path):
     try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        directory = "versions/web_static_{}.tgz".\
-            format(datetime.now().strftime("%Y%m%d%H%M%S"))
-        local('tar -cvzf {} web_static'.format(directory))
-        return directory
+        if not os.path.exists(archive_path):
+            return False
+        put(archive_path, '/tmp/')
+        archive_name = archive_path.split('/')[-1]
+        upload_folder = '/data/web_static/releases/{}'.format(archive_name[:-3])
+        run('tar -xzf /tmp/{} -C {}'.format(archive_name, upload_folder))
+        run('rm /tmp/{}'.format(archive_name))
+        run('ln -sf /data/web_static/releases/{} /data/web_static/current'.format(archive_name[:-3]))
+        return True
     except Exception:
-        return None
+        return False
