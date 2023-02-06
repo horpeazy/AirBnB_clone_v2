f42b485c1cd700e616c035bc7bb92d8c2866c22f
Iyamu Hope <iyamuhope.nosa647@gmail.com>
2023-02-06 01:59:41 +0100

    feat: simple server

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,3 +18,6 @@ def states_list():
     """ renders all states in the database"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
